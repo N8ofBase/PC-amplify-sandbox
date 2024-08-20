3a680d63935ba0afa5dfdd6c710eca41895ea375
N8ofBase <n8ofbase@gmail.com>
2024-08-20 08:57:45 -0500

    Update App.tsx
    semis

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Authenticator } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css'
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
